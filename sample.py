e365cf969bc15b8f6a7153d8724960463ed8179e
bharadwajrembar <bharadwajrembarr@gmail.com>
2017-01-12 15:07:00 +0530

    update another sample.py

--- sample.py
+++ sample.py
@@ -2,4 +2,4 @@ import os
 
 #This is a comment
 os.system('sudo /bin/bash -c \'mkdir ~/test\'')
-os.system('sudo /bin/bash -c \'cd ~/test && touch something.txt && echo \'worked\' >> another_test.txt\'')
+os.system('sudo /bin/bash -c \'cd ~/test && touch something.txt && echo \'worked\' >> another_not_test.txt\'')
